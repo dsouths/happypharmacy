17c9fc40657244f774b7af930751069f623208b7
dsouths <dsouths@hotmail.com>
2023-08-12 19:16:30 +0000

    amended home views

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path(
+        '',
+        views.all_products,
+        name='products'),
+    path(
+        '<int:product_id>/',
+        views.product_detail,
+        name='product_detail')
 
 ]
