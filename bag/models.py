2f52ca263841fcb701333eea9766f4427489459a
dsouths <dsouths@hotmail.com>
2023-08-12 19:38:36 +0000

    added urlpatterns for bag models

--- bag/models.py
+++ bag/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from django.urls import path
+from . import views
 
-# Create your models here.
+urlpatterns = [
+    path('', views.view_bag, name='view_bag'),
+    
+]
