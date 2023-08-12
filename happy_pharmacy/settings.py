b1787bdc47b04ee8268446d091faf509cf12b127
dsouths <dsouths@hotmail.com>
2023-08-12 16:46:59 +0000

    created index.html

--- happy_pharmacy/settings.py
+++ happy_pharmacy/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'happy_pharmacy',
-    'home'
+    'home',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
