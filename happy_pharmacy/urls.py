b710b3169569647f75dd25948f39c4533425ab65
dsouths <dsouths@hotmail.com>
2023-08-12 19:03:47 +0000

    added products.html

--- happy_pharmacy/urls.py
+++ happy_pharmacy/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product 
+from .models import Product
 
 # Create your views here.
 def all_products(request):
