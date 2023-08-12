61ab77a42e321ecc9298f0cd1e2e3d9d8b6a487a
dsouths <dsouths@hotmail.com>
2023-08-12 17:49:40 +0000

    added all product views

--- products/models.py
+++ products/models.py
@@ -1,15 +1,65 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Category(models.Model):
-    name = models.CharField(max_length = 254)
-    friendly_name = models.CharField(max_length = 254, null = True, blank = True)
-
+    verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
     def get_friendly_name(self):
         return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    ingredients = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    directions = models.TextField()
+    rating = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    stock_level = models.IntegerField(default=10)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductReview(models.Model):
+    product = models.ForeignKey(
+        Product,
+        related_name='reviews',
+        on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User,
+        related_name='reviews',
+        on_delete=models.CASCADE)
+    content = models.TextField(blank=True, null=True)
+    title = models.CharField(max_length=10000)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Wishlist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return f'{self.user.username} Wishlist'
