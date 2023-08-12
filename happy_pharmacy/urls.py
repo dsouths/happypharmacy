caeab7e1a558f1f3089094716379531746f3571c
dsouths <dsouths@hotmail.com>
2023-08-12 20:42:29 +0000

    amended happy pharmacy urls

--- happy_pharmacy/urls.py
+++ happy_pharmacy/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,7 +25,10 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('blog/', include('blog.urls')),
+    path('contact/', include('contact.urls')),
     
 ]
 
