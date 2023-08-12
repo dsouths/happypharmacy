e128663776dbf04b5fd188df69d6ef50694c383b
dsouths <dsouths@hotmail.com>
2023-08-12 20:33:25 +0000

    added checkout app

--- happy_pharmacy/urls.py
+++ happy_pharmacy/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
+    path('profile/', include('profiles.urls')),
     
 ]
 
