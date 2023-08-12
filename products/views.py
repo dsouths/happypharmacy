5a1c05c1bb8498d1e3ce5d8358d01fec2f10fda0
dsouths <dsouths@hotmail.com>
2023-08-12 20:07:01 +0000

    added add product function in views

--- products/views.py
+++ products/views.py
@@ -19,3 +19,30 @@ def all_products(request):
     categories = None
     sort = None
     direction = None
+
+@login_required
+def add_product(request):
+    """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(
+                request,
+                'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
