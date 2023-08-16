efd2e7ac71151d7ffe5d6b3da523291a9ada45a3
dsouths <dsouths@hotmail.com>
2023-08-16 13:40:32 +0000

    added contact.html & changed email backend to development for now

--- happy_pharmacy/settings.py
+++ happy_pharmacy/settings.py
@@ -215,14 +215,6 @@ STRIPE_SECRET_KEY = 'sk_test_51Nbmd8IOLnsla5tCFplQVYC92do8keVsAkHwBXAnKTQJbFtHVh
 
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 
-if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'info.happypharmacy@gmail.com'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'info.happypharmacy@gmail.com'
