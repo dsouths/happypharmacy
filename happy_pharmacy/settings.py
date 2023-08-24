181ce3bd66a9e03fe29bdb36ca4488c213f5f7c6
dsouths <dsouths@hotmail.com>
2023-08-24 21:39:57 +0000

    amended staticfiles storage in settings.py

--- happy_pharmacy/settings.py
+++ happy_pharmacy/settings.py
@@ -191,7 +191,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
