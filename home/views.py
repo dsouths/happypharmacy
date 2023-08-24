d5aa47a5609200736aa0c09dfbb2ba17794eb761
dsouths <dsouths@hotmail.com>
2023-08-24 20:01:35 +0000

    hardcoded cloudinary links for media/images

--- home/views.py
+++ home/views.py
@@ -10,5 +10,6 @@ def index(request):
     testimonials = Testimonial.objects.filter(is_approved=True).order_by('-created_at')[:2] 
     context = {
         'testimonials': testimonials,
+        'hero_logo_url': 'https://res.cloudinary.com/dghprmi1e/image/upload/v1692736553/happypharmacy/hplogo.png'
     }
     return render(request, 'home/index.html', context)
