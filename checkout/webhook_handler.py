f6ea9b1acc5a6fc17468b23629938f586c30d0e1
dsouths <dsouths@hotmail.com>
2023-08-29 20:02:11 +0000

    amended checkout model & webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -97,6 +97,7 @@ class StripeWH_Handler:
                     street_address2__iexact=shipping_details.address.line2,
                     county__iexact=shipping_details.address.state,
                     grand_total=grand_total,
+                    original_bag=bag,
                     stripe_pid=pid,
                 )
                 order_exists = True
@@ -124,6 +125,7 @@ class StripeWH_Handler:
                     street_address1=shipping_details.address.line1,
                     street_address2=shipping_details.address.line2,
                     county=shipping_details.address.state,
+                    original_bag=bag,
                     stripe_pid=pid,
                 )
                 for item_id, item_data in json.loads(bag).items():
