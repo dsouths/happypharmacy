8a91fa021c2a33e79c3cb63e7750b6a8e2c80ff8
dsouths <dsouths@hotmail.com>
2023-08-29 19:57:12 +0000

    amended broken faq links

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -97,7 +97,6 @@ class StripeWH_Handler:
                     street_address2__iexact=shipping_details.address.line2,
                     county__iexact=shipping_details.address.state,
                     grand_total=grand_total,
-                    original_bag=bag,
                     stripe_pid=pid,
                 )
                 order_exists = True
@@ -125,7 +124,6 @@ class StripeWH_Handler:
                     street_address1=shipping_details.address.line1,
                     street_address2=shipping_details.address.line2,
                     county=shipping_details.address.state,
-                    original_bag=bag,
                     stripe_pid=pid,
                 )
                 for item_id, item_data in json.loads(bag).items():
