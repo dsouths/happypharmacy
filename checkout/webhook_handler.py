7caeb3a7ff87f6138eed32de88c73949affe85bc
dsouths <dsouths@hotmail.com>
2023-08-29 19:29:56 +0000

    adjust webhooks handler for stripe

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -57,9 +57,10 @@ class StripeWH_Handler:
             intent.latest_charge
         )
 
-        billing_details = intent.charges.data[0].billing_details
+        billing_details = stripe_charge.billing_details # updated
         shipping_details = intent.shipping
-        grand_total = round(intent.charges.data[0].amount / 100, 2)
+        grand_total = round(stripe_charge.amount / 100, 2) # updated
+        
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
