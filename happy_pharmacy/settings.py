67ac7f09ff598efdf7c1eb23df9e3f2051c90483
dsouths <dsouths@hotmail.com>
2023-08-31 20:22:52 +0000

    static files issue

--- happy_pharmacy/settings.py
+++ happy_pharmacy/settings.py
@@ -196,8 +196,8 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
